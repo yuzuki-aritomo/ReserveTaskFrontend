ba2298acde1899f45aafa70a5956e6a78308f4cb
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2021-12-08 19:58:56 +0900

    add prettier

--- next-app/.eslintrc.js
+++ next-app/.eslintrc.js
@@ -1,10 +1,6 @@
 module.exports = {
-  extends: [
-    'next',
-    'next/core-web-vitals',
-  ],
+  extends: ['next', 'next/core-web-vitals', 'prettier'],
   rules: {
-    'semi': 'error',
     'import/prefer-default-export': 'off',
     'newline-before-return': 'error',
     'no-console': 'warn',
