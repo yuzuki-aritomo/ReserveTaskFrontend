23e31ff2bd33f7d044f7dfe15e3c43922f4cea54
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2021-12-08 19:35:28 +0900

    add eslint

---
+++ next-app/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    'next',
+    'next/core-web-vitals',
+  ],
+  rules: {
+    'semi': 'error',
+    'import/prefer-default-export': 'off',
+    'newline-before-return': 'error',
+    'no-console': 'warn',
+    'no-var': 'error',
+  },
+};
