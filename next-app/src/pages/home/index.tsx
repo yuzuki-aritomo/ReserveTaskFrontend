a3da8ea5ffb4e9e296832ec13f5eb20caef507d0
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2021-12-09 12:53:29 +0900

    components 分解

--- next-app/src/pages/home/index.tsx
+++ next-app/src/pages/home/index.tsx
@@ -1,37 +1,14 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
-import { SignOutApi } from 'src/api/auth/SignOutApi'
-import { Row, Col, Card, Form, Button } from "react-bootstrap"
+import { SignOut } from 'src/components/auth/SignOut'
 
 const HomePage: NextPage = () => {
-
-  const SignOutSubmit = async (event: any) => {
-    event.preventDefault();
-    try{
-      await SignOutApi()
-    }catch(e){
-      console.error(e)
-    }
-  }
 
   return(
     <div>
       <Link href="/">Top</Link>
       <h1>sign out</h1>
-      <Row className="justify-content-md-center mt-4">
-        <Col md={8}>
-          <Card>
-            <Card.Header>Sign Out</Card.Header>
-            <Card.Body>
-              <Form onSubmit={SignOutSubmit}>
-                <Button className="mt-2" variant="primary" type="submit">
-                  Sign Out
-                </Button>
-              </Form>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+      <SignOut />
     </div>
   )
 }
