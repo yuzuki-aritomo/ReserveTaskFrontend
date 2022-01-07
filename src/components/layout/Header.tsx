95f0cf2d19099b7017c196e8874529613a914c7c
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2022-01-07 14:52:05 +0900

    add reception button

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -16,9 +16,14 @@ const Header: FC = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             { user ? // login
+            <>
               <Link href='/home' passHref>
                 <Nav.Link>Home</Nav.Link>
               </Link>
+              <Link href='/receptions' passHref>
+                <Nav.Link>Receptions</Nav.Link>
+              </Link>
+            </>
               : // not login
               <>
                 <Link href='/sign_up' passHref>
