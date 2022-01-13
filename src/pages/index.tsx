4ae1dfc621402a10e76543f54a49711d02067037
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2022-01-13 14:28:41 +0900

    add home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,38 @@
 import type { NextPage } from 'next'
-import Link from 'next/link'
+import styles from 'styles/top.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div></div>
+    <main className={styles.main}>
+      <h1 className={styles.title}>
+        Welcome to <p className={styles.color}>Financial Planner App</p>
+      </h1>
+      <div className={styles.grid}>
+        <a
+          href="https://yuzuki-aritomo.github.io/ReserveTaskBackend/dist/index.html"
+          className={styles.card}
+        >
+          <h2>Api reference &rarr;</h2>
+          <p>Github Source code</p>
+        </a>
+
+        <a
+          href="https://github.com/yuzuki-aritomo/ReserveTaskFrontend"
+          className={styles.card}
+        >
+          <h2>Front end  &rarr;</h2>
+          <p>Github Source code</p>
+        </a>
+
+        <a
+          href="https://github.com/yuzuki-aritomo/ReserveTaskBackend"
+          className={styles.card}
+        >
+          <h2>Back end &rarr;</h2>
+          <p>Github Source code</p>
+        </a>
+      </div>
+    </main>
   )
 }
 
